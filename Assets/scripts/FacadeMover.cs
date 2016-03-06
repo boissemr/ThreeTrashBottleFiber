6fa27f4a39b4f9850b6e82a70939c1878304946c
Matt Boisseau <boissemr@miamioh.edu>
2016-03-06 01:46:54 -0500

    commit before calling it a night

---
+++ Assets/scripts/FacadeMover.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacadeMover : MonoBehaviour {
+
+	public float speed;
+
+	void Update () {
+		transform.position += Vector3.back * speed * Time.deltaTime;
+	}
+}
